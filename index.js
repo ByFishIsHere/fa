535ef5e913f6cd1ed08762353afbca12ea51d704
ByFishIsHere <44239946+ByFishIsHere@users.noreply.github.com>
2018-10-29 20:36:31 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+const token = process.env.token
+
+bot.on('message', function (message)    {
+    if (message.content === '!bière')    {
+        message.channel.send(':beers: **Tiens voilà une bonne bière mon ami(e)** :beers:')
+    }
+    if (message.content === '!coca')    {
+        message.channel.send(':tumbler_glass: **Tiens voilà un bon coca sans bulles** :tumbler_glass: ')
+    }
+    if (message.content === '!cidre')    {
+        message.channel.send('**Et deux jus de pomme qui piquent !**')
+    }
+    if (message.content === '!space')    {
+        message.delete()
+        message.channel.send('``` ```')
+    }
+})
+bot.login(token);
