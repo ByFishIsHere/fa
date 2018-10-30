a9fd01afe3710221b26de36bcea6bc06fe396680
ByFishIsHere <44239946+ByFishIsHere@users.noreply.github.com>
2018-10-30 14:00:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const token = process.env.token
 
 bot.on('message', function (message)    {
     if (message.content === '!bière')    {
+        message.delete()
         message.channel.send(':beers: **Tiens voilà une bonne bière mon ami(e)** :beers:')
     }
     if (message.content === '!coca')    {
+        message.delete()
         message.channel.send(':tumbler_glass: **Tiens voilà un bon coca sans bulles** :tumbler_glass: ')
     }
     if (message.content === '!cidre')    {
+        message.delete()
         message.channel.send('**Et deux jus de pomme qui piquent !**')
     }
     if (message.content === '!space')    {
